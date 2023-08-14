3faf3c1258cb206b95a72f269b1d1af9749549b7
Vrba Martin <Martin.Vrba@t-mobile.cz>
2023-08-14 11:00:41 +0200

    cviceni 7-2

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import javax.persistence.*;
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private long id;
+    private String name;
+    private double price;
+    private String description;
+
+    public Product() {}
+
+    public Product(long id, String name, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
